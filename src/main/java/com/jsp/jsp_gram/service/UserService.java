3abf145862b0a06b209ca4216bd708d987349466
Poojapatlll <poojapatil814745@gmail.com>
2025-01-30 21:07:43 -0800

    Updated Project

---
+++ src/main/java/com/jsp/jsp_gram/service/UserService.java
@@ -0,0 +1,72 @@
+package com.jsp.jsp_gram.service;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+
+import com.jsp.jsp_gram.dto.User;
+import com.jsp.jsp_gram.helper.AES;
+import com.jsp.jsp_gram.helper.EmailSender;
+import com.jsp.jsp_gram.repository.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository repository;
+
+	@Autowired
+	EmailSender emailSender;
+
+	public String loadRegister(ModelMap map, User user) {
+		map.put("user", user);
+		return "register.html";
+	}
+
+	public String register(User user, BindingResult result, HttpSession session) {
+		if (!user.getPassword().equals(user.getConfirmpassword()))
+			result.rejectValue("confirmpassword", "error.confirmpassword", "Passwords not Matching");
+
+		if (repository.existsByEmail(user.getEmail()))
+			result.rejectValue("email", "error.email", "Email already Exists");
+
+		if (repository.existsByMobile(user.getMobile()))
+			result.rejectValue("mobile", "error.mobile", "Mobile Number Already Exists");
+
+		if (repository.existsByUsername(user.getUsername()))
+			result.rejectValue("username", "error.username", "Username already Taken");
+
+		if (result.hasErrors())
+			return "register.html";
+		else {
+			user.setPassword(AES.encrypt(user.getPassword()));
+			int otp = new Random().nextInt(100000, 1000000);
+			user.setOtp(otp);
+			System.err.println(otp);
+			 emailSender.sendOtp(user.getEmail(), otp, user.getFirstname());
+			repository.save(user);
+			session.setAttribute("pass", "Otp Sent Success");
+			return "redirect:/otp/" + user.getId();
+		}
+	}
+
+	public String verifyOtp(int otp, int id, HttpSession session) {
+		User user = repository.findById(id).get();
+		if (user.getOtp() == otp) {
+			user.setVerified(true);
+			user.setOtp(0);
+			repository.save(user);
+			session.setAttribute("pass", "Account Created Success");
+			return "redirect:/login";
+		} else {
+			session.setAttribute("fail", "Invalid Otp, Try Again!!!");
+			return "redirect:/otp/" + id;
+		}
+
+	}
+}
